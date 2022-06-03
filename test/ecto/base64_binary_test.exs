0a66f1c01f8b76198a782e4031313c0f51ae7c75
Minhajuddin Khaja <minhajuddin.k@gmail.com>
2022-06-03 01:45:09 -0400

    Rename

--- test/ecto/base64_binary_test.exs
+++ test/ecto/base64_binary_test.exs
@@ -5,9 +5,9 @@ defmodule Ecto.PrefixedID.Base64BinaryTest do
   alias Ecto.PrefixedID.Base64Binary
 
   @prefix "cat"
-  @binary_uuid <<37, 49, 145, 228, 118, 254, 213, 251, 100, 90, 109, 69, 225, 182, 7, 193, 150,
-                 185, 187, 36, 242, 70, 90, 117>>
-  @prefixed_base64_uuid "cat_JTGR5Hb-1ftkWm1F4bYHwZa5uyTyRlp1"
+  @binary_value <<37, 49, 145, 228, 118, 254, 213, 251, 100, 90, 109, 69, 225, 182, 7, 193, 150,
+                  185, 187, 36, 242, 70, 90, 117>>
+  @prefixed_base64_value "cat_JTGR5Hb-1ftkWm1F4bYHwZa5uyTyRlp1"
   @opts Base64Binary.init(prefix: @prefix, byte_size: 24)
 
   test "init" do
@@ -49,30 +49,30 @@ defmodule Ecto.PrefixedID.Base64BinaryTest do
   end
 
   test "cast" do
-    assert Base64Binary.cast(@prefixed_base64_uuid, @opts) == {:ok, @prefixed_base64_uuid}
-    assert Base64Binary.cast(@binary_uuid, @opts) == {:ok, @prefixed_base64_uuid}
+    assert Base64Binary.cast(@prefixed_base64_value, @opts) == {:ok, @prefixed_base64_value}
+    assert Base64Binary.cast(@binary_value, @opts) == {:ok, @prefixed_base64_value}
 
     assert Base64Binary.cast("xcat_J7hJAIFSQT-E5e5LhWUHtw", @opts) == :error
     assert Base64Binary.cast("cat.J7hJAIFSQT-E5e5LhWUHtw", @opts) == :error
-    assert Base64Binary.cast(<<1>> <> @binary_uuid, @opts) == :error
+    assert Base64Binary.cast(<<1>> <> @binary_value, @opts) == :error
   end
 
   test "dump" do
-    assert Base64Binary.dump(@prefixed_base64_uuid, :dumper, @opts) == {:ok, @binary_uuid}
+    assert Base64Binary.dump(@prefixed_base64_value, :dumper, @opts) == {:ok, @binary_value}
     assert Base64Binary.dump(nil, :dumper, @opts) == {:ok, nil}
     assert Base64Binary.dump("xcat_J7hJAIFSQT-E5e5LhWUHtw", :dumper, @opts) == :error
     assert Base64Binary.dump(33, :dumper, @opts) == :error
   end
 
   test "load" do
-    assert Base64Binary.load(@binary_uuid, :loader, @opts) == {:ok, @prefixed_base64_uuid}
+    assert Base64Binary.load(@binary_value, :loader, @opts) == {:ok, @prefixed_base64_value}
     assert Base64Binary.load(nil, :loader, @opts) == {:ok, nil}
     assert Base64Binary.load(33, :loader, @opts) == :error
   end
 
   test "autogenerate" do
-    prefixed_base64_uuid = Base64Binary.autogenerate(@opts)
-    assert prefixed_base64_uuid =~ ~r(cat_[a-zA-Z0-9_-]+)
-    assert String.length(prefixed_base64_uuid) == 3 + 1 + 32
+    prefixed_base64_value = Base64Binary.autogenerate(@opts)
+    assert prefixed_base64_value =~ ~r(cat_[a-zA-Z0-9_-]+)
+    assert String.length(prefixed_base64_value) == 3 + 1 + 32
   end
 end
